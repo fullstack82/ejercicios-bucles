283373daec8ebdcfb79eee6c76533121476f31bc
fullstack82 <fullstack8210@gmail.com>
2021-01-29 13:34:10 +0100

    acabando el ejercicio 4

--- index.php
+++ index.php
@@ -69,14 +69,24 @@ if (isset($_GET['numero1']) && isset($_GET['numero2'])) {
 * que nos lleguen por la url($_GET);
 */
 
-if(isset($_GET['numero1']) && isset($_GET['numero2'])){
+if (isset($_GET['numero1']) && isset($_GET['numero2'])) {
     $numero1 = $_GET['numero1'];
     $numero2 = $_GET['numero2'];
 
-
-    for($i = $numero1; $i <= $numero2; $i++){
-        echo "<h4>$i</h4>";
+    if ($numero1 < $numero2) {
+        for ($i = $numero1; $i <= $numero2; $i++) {
+            echo "<h4>$i</h4>";
+        }
+    }else{
+        echo "<h3>El numero 1 debe ser menor que el numero 2</h3>";
     }
-}else{
+} else {
     echo "<h1>Los parametros get no existen</h1>";
 }
+
+
+
+/*  Ejercico 5 = 
+
+
+*/
