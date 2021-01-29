e8d29b045b08d2cbd87143e732f0045137e49ca3
fullstack82 <fullstack8210@gmail.com>
2021-01-29 10:08:36 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+// Ejercicio 1 = Crea dos variables cuyos nombres sean pais y continente e imprimir por pantalla; Poner en un comentario que tipo de dato tienen
+
+$pais = "España";
+$continente = "Europa";
+
+echo $pais."<br/>";
+echo $continente."<br/>";
+
+echo gettype($pais)."<br/>";
+echo gettype($continente)."<br/>";
